2e580bf2692de9fb58c0060839dbd18af8db09f8
carlosaux <52382443+carlosaux@users.noreply.github.com>
2021-04-10 11:54:12 -0500

    Bienvenido

---
+++ Modelo/Conexion.php
@@ -0,0 +1,6 @@
+<?php
+    $mysqli = new mysqli('localhost','root', '', 'aguitadecoco');
+    if($mysqli->connect_errno):
+        echo "Error al conectase con MySQL debido al error ".$mysqli->connect_error;
+    endif;
+?>
---
+++ Vista/index.php
@@ -0,0 +1,63 @@
+<?php 
+  include_once ('navSuperior.php');
+?>
+<!doctype html>
+<html lang="es">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" l>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../CSS/StyleUser.css">
+  <title>Aguita de coco</title>
+</head>
+<body>
+  <div class="d-flex toggled" id="wrapper">    
+    <div id="page-content-wrapper">
+      <div class="panel-group hide" id="contenedor"><div class="panel panel-primary">
+            <div class="panel-heading" id="titulo"></div>
+            <div class="panel-body">
+                
+                <div class="form-group" id="contenido" class="Contenido">        
+                    
+                </div>
+            </div>
+        </div>             
+      </div>      
+    </div>
+  </div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <input type="hidden" id="pagina" value="index" name="editar"/>
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
+    <!-- Librearía para las funcionalidades de la tabla -->
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.28.2/sweetalert2.all.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+      
+
+
+
+  <script src="../js/funcionesJquery.js"></script>
+  
+  
+ 
+  <script type="text/javascript">
+
+   
+    $(document).ready(inicio)
+    
+  </script>
+</body>
+
+</html>
