3681d5222bec9babcf7c60a5d5778401de1b39ac
carlosaux <52382443+carlosaux@users.noreply.github.com>
2021-04-11 12:14:30 -0500

    actualizacion

--- PHP/Producto/productos.php
+++ PHP/Producto/productos.php
@@ -21,9 +21,6 @@
         <i class="ion ion-clipboard"></i>
         <!-- tools box -->
         <div class="pull-right box-tools">
-            <button class="btn btn-info btn-sm" id="nuevo"  data-toggle="tooltip" title="Nuevo producto"><i class="fa fa-plus" aria-hidden="true"></i></button> 
-            <button class="btn btn-info btn-sm btncerrar"  data-toggle="tooltip" title="Ocultar"><i class="fa fa-times"></i></button>
-
         </div><!-- /. tools -->
                     
     </div><!-- /.box-header -->
